0543b1a98a2f5ecaa209206538143b6aa4f04cb5
Mohammed 'Moniac' Mulazada <thisismoniac@gmail.com>
2019-12-29 22:30:07 +0100

    fix issue

--- screens/CompletedTodosOverviewScreen.js
+++ screens/CompletedTodosOverviewScreen.js
@@ -1,10 +1,75 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, AsyncStorage, FlatList } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { Card, Button as NativeButton } from "react-native-paper";
+import { ZeroInboxMessage } from "./TodoOverviewScreen";
 
 export default function CompletedTodosOverviewScreen() {
+  const [todos, setTodos] = useState([]);
+  console.log(todos);
+
+  async function fetchTodos() {
+    try {
+      const allItems = await AsyncStorage.getItem("todos");
+
+      if (allItems) {
+        const parsedItems = await JSON.parse(allItems);
+        const filteredItems = parsedItems.filter(
+          todo => todo.completed === true
+        );
+        console.log(parsedItems, "from complete");
+        setTodos(filteredItems);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function removeFromArray(id) {
+    const allItems = await AsyncStorage.getItem("todos");
+    const parsedItems = await JSON.parse(allItems);
+    const ted = parsedItems.filter(t => t.id !== id);
+    console.log(ted, "HEREEE");
+    await AsyncStorage.setItem("todos", JSON.stringify(ted));
+    fetchTodos();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+      <NavigationEvents onWillFocus={payload => fetchTodos()} />
+      {todos.length > 0 ? (
+        <FlatList
+          style={styles.list}
+          data={todos.map((todo, i) => ({
+            title: todo.name,
+            id: todo.id,
+            key: `${todo.name} - ${i}`
+          }))}
+          renderItem={({ item, index }) => {
+            return (
+              <Card
+                style={{
+                  ...styles.Card,
+                  backgroundColor: index % 2 ? "#f9f9f9" : "#fff"
+                }}
+              >
+                <Card.Title title={item.title} titleStyle={styles.title} />
+                <Card.Actions>
+                  <NativeButton onPress={() => removeFromArray(item.id)}>
+                    Delete
+                  </NativeButton>
+                </Card.Actions>
+              </Card>
+            );
+          }}
+        />
+      ) : (
+        <View style={styles.emptyState}>{ZeroInboxMessage}</View>
+      )}
     </View>
   );
 }
@@ -12,7 +77,18 @@ export default function CompletedTodosOverviewScreen() {
 const styles = StyleSheet.create({
   container: {
     padding: 16,
+    paddingLeft: 0,
+    paddingRight: 0,
     marginTop: 60,
     marginBottom: 40
+  },
+  title: {
+    textDecorationLine: "line-through",
+    opacity: 0.8
+  },
+  emptyState: {
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
--- screens/TodoOverviewScreen.js
+++ screens/TodoOverviewScreen.js
@@ -10,6 +10,7 @@ import {
   Keyboard
 } from "react-native";
 import { Card, Button as NativeButton } from "react-native-paper";
+import { NavigationEvents } from "react-navigation";
 
 const ZeroInboxMessages = [
   "Nothing. You have achieved peace.",
@@ -24,14 +25,11 @@ function getRandomZeroInboxMessage() {
   );
 }
 
-const ZeroInboxMessage = getRandomZeroInboxMessage();
+export const ZeroInboxMessage = getRandomZeroInboxMessage();
 
 export default function TodoOverviewScreen() {
   const [todos, setTodos] = useState([]);
   const [inputText, setInputText] = useState("");
-
-  const hasTodos =
-    todos && todos.filter(todo => todo.completed === false).length > 0;
 
   async function fetchTodos() {
     try {
@@ -39,6 +37,7 @@ export default function TodoOverviewScreen() {
 
       if (allItems) {
         const parsedItems = await JSON.parse(allItems);
+        console.log(parsedItems, "PARSED");
         setTodos(parsedItems);
       }
     } catch (err) {
@@ -50,6 +49,9 @@ export default function TodoOverviewScreen() {
     fetchTodos();
   }, []);
 
+  const hasTodos =
+    todos && todos.filter(todo => todo.completed === false).length > 0;
+
   useEffect(() => {
     saveToDos();
     setInputText("");
@@ -57,7 +59,10 @@ export default function TodoOverviewScreen() {
   }, [todos]);
 
   async function saveToDos() {
-    await AsyncStorage.setItem("todos", JSON.stringify(todos));
+    console.log("saved", todos);
+    if (todos.length) {
+      await AsyncStorage.setItem("todos", JSON.stringify(todos));
+    }
   }
 
   async function clearAsyncStorage() {
@@ -83,11 +88,12 @@ export default function TodoOverviewScreen() {
   function removeFromArray(id) {
     const filteredTodos = todos.filter(todo => todo.id !== id);
 
-    setTodos(filteredTodos);
+    setTodos([...filteredTodos]);
   }
 
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillFocus={payload => fetchTodos()} />
       <View style={styles.header}>
         <TextInput
           autoCapitalize="sentences"
@@ -206,179 +212,3 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1
   }
 });
-
-// import React, { useState, useEffect } from "react";
-// import {
-//   StyleSheet,
-//   Text,
-//   View,
-//   Button,
-//   AsyncStorage,
-//   FlatList,
-//   TextInput,
-//   Keyboard
-// } from "react-native";
-// import { Card, Button as NativeButton } from "react-native-paper";
-
-// export default function TodoOverviewScreen() {
-//   const [todos, setTodos] = useState([]);
-//   const [inputText, setInputText] = useState("");
-
-//   const hasTodos = todos && todos.length > 0;
-
-//   async function fetchTodos() {
-//     try {
-//       const allItems = await AsyncStorage.getItem("todos");
-
-//       if (allItems) {
-//         setTodos(JSON.parse(allItems));
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-
-//   useEffect(() => {
-//     fetchTodos();
-//   }, []);
-
-//   useEffect(() => {
-//     console.log(todos, "USE");
-//     setInputText("");
-//     saveToDos();
-//     Keyboard.dismiss();
-//   }, [todos]);
-
-//   async function saveToDos() {
-//     console.log(todos, "TO SAVE");
-//     await AsyncStorage.setItem("todos", JSON.stringify(todos));
-//   }
-
-//   async function clearAsyncStorage() {
-//     AsyncStorage.clear();
-//     setTodos([]);
-//   }
-
-//   function markAsCompleted(index) {
-//     const completedTodo = todos[index];
-//     completedTodo.completed = true;
-//     const updated = todos;
-//     updated[index] = completedTodo;
-//     console.log(updated, "CHANGED", index);
-
-//     setTodos([...updated]);
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.header}>
-//         <TextInput
-//           autoCapitalize="sentences"
-//           placeholder="What to do?"
-//           blurOnSubmit={false}
-//           value={inputText}
-//           style={styles.input}
-//           onChangeText={value => setInputText(value)}
-//           onSubmitEditing={e =>
-//             setTodos(
-//               todos =>
-//                 (todos =
-//                   inputText.trim().length > 0 && todos !== null
-//                     ? todos.concat([{ name: inputText, completed: false }])
-//                     : [...todos])
-//             )
-//           }
-//         />
-//       </View>
-//       {hasTodos ? (
-//         <FlatList
-//           style={styles.list}
-//           data={todos
-//             .filter(t => t.completed === false)
-//             .map((todo, i) => ({
-//               title: todo.name,
-//               key: `${todo.name} - ${i}`
-//             }))}
-//           renderItem={({ item, index }) => {
-//             return (
-//               <Card
-//                 style={{
-//                   ...styles.Card,
-//                   backgroundColor: index % 2 ? "#f9f9f9" : "#fff"
-//                 }}
-//               >
-//                 <Card.Title title={item.title} />
-//                 <Card.Actions>
-//                   <NativeButton onPress={() => removeFromArray(index)}>
-//                     Delete
-//                   </NativeButton>
-//                   <NativeButton onPress={() => markAsCompleted(index)}>
-//                     Complete
-//                   </NativeButton>
-//                 </Card.Actions>
-//               </Card>
-//             );
-//           }}
-//         />
-//       ) : (
-//         <Text>Nothing. You have reached peace.</Text>
-//       )}
-//       {hasTodos && (
-//         <Button
-//           style={styles.clear}
-//           title={"Clear"}
-//           onPress={() => clearAsyncStorage()}
-//         />
-//       )}
-//     </View>
-//   );
-
-//   function removeFromArray(index) {
-//     const values = todos;
-//     values.splice(index, 1);
-
-//     setTodos([...values]);
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "space-evenly",
-//     marginTop: 60,
-//     marginBottom: 40,
-//     height: "100%"
-//   },
-//   list: {
-//     marginTop: 50,
-//     height: "100%",
-//     width: "100%"
-//   },
-//   header: {
-//     position: "absolute",
-//     top: 0,
-//     width: "100%",
-//     marginLeft: "auto",
-//     marginRight: "auto"
-//   },
-//   clear: {
-//     marginBottom: 40,
-//     paddingBottom: 400
-//   },
-//   Card: {
-//     // marginTop: 8
-//   },
-//   input: {
-//     width: "100%",
-//     marginLeft: "auto",
-//     marginRight: "auto",
-//     padding: 4,
-//     minHeight: 40,
-//     paddingLeft: 16,
-//     paddingRight: 16,
-//     borderBottomColor: "#f1f1f1",
-//     borderBottomWidth: 1
-//   }
-// });
