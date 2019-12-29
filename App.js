a3ff059cd8a7e822c91b85f66f7e295d699a9141
Mohammed 'Moniac' Mulazada <thisismoniac@gmail.com>
2019-12-29 02:09:49 +0100

    fix error

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import React from "react";
-import { View } from "react-native";
 import TodoOverviewScreen from "./screens/TodoOverviewScreen";
 import CompletedTodosOverviewScreen from "./screens/CompletedTodosOverviewScreen";
 import { createAppContainer } from "react-navigation";
-import {
-  createBottomTabNavigator,
-  createMaterialTopTabNavigator
-} from "react-navigation-tabs";
-
-function App() {
-  return (
-    <View style={{ height: "100%" }}>
-      <TodoOverviewScreen />
-    </View>
-  );
-}
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
 
 const TabNavigator = createMaterialTopTabNavigator(
   {
